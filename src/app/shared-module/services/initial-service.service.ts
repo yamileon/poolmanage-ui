ef24d7597db57691fc62c246ad015d4ea5ebb59d
yamileon <yamileon.xfz@gmail.com>
2019-07-08 13:37:41 +0100

    split call url into ip var in env folder

--- src/app/shared-module/services/initial-service.service.ts
+++ src/app/shared-module/services/initial-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { DataRequestService } from 'src/app/shared-module/services/data-request.service';
 import { Observable } from 'rxjs';
 import { Iuser } from 'src/app/users/interfaces/iuser';
+import { environment as env } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -18,37 +19,37 @@ export class InitialServiceService {
   }
 
   getUserData(): Observable<Iuser[]> {
-    return this.req.get<Iuser[]>('http://localhost:8080/user/all');
+    return this.req.get<Iuser[]>(env.url+':8080/user/all');
   }
 
   getOneUser(username: Iuser): Observable<Iuser> {
-    let urlString = 'http://localhost:8080/user/byUsername?username=' + username.username;
+    let urlString = env.url+':8080/user/byUsername?username=' + username.username;
     return this.req.getOne<Iuser>(urlString);
 
   }
 
   postUserData<Iuser>(user: Iuser): Observable<Iuser> {
-    return this.req.post<Iuser>('http://localhost:8080/user/create', user);
+    return this.req.post<Iuser>(env.url+':8080/user/create', user);
   }
 
   changeMultiple<Iuser>(username: string, user: Iuser): Observable<Iuser> {
-    return this.req.put<Iuser>('http://localhost:8080/user/updateUser/' + username + '/', user);
+    return this.req.put<Iuser>(env.url+':8080/user/updateUser/' + username + '/', user);
   }
 
   incrementWin(username: string): Observable<Iuser> {
-    return this.req.putIncrement('http://localhost:8080/user/updateUserInfo/win/' + username, JSON.stringify(""));
+    return this.req.putIncrement(env.url+':8080/user/updateUserInfo/win/' + username, JSON.stringify(""));
   }
 
   incrementLoss(username: string): Observable<Iuser> {
-    return this.req.putIncrement('http://localhost:8080/user/updateUserInfo/loss/' + username, JSON.stringify(""));
+    return this.req.putIncrement(env.url+':8080/user/updateUserInfo/loss/' + username, JSON.stringify(""));
   }
 
   incrementGamesPlayed(username: string): Observable<Iuser> {
-    return this.req.putIncrement('http://localhost:8080/user/updateUserInfo/gamesPlayed/' + username, JSON.stringify(""));
+    return this.req.putIncrement(env.url+':8080/user/updateUserInfo/gamesPlayed/' + username, JSON.stringify(""));
   }
 
   deleteUserById<Iuser>(id: string): Observable<Iuser> {
     console.log(id);
-    return this.req.delete<Iuser>(`http://localhost:8080/user/deleteUser?id=` + id);
+    return this.req.delete<Iuser>(env.url+':8080/user/deleteUser?id=' + id);
   }
 }
